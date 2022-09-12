edd7dcb5cab8995f9640ae0e43ed9c7a41306728
Kalki Nethaji <kalki@skaas.guru>
2022-09-12 23:27:23 +0530

    Added ViewChild

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,16 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
   @Output() searchSubmitted: EventEmitter<string> = new EventEmitter<string>();
+
+  @ViewChild('searchInput') searchInput!: ElementRef;
+
+  ngAfterViewInit(): void {
+    console.log(this.searchInput);
+  }
 }
