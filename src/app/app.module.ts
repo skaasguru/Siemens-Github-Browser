d16e47d577998e98c57f8990e2486ed147b10ba0
Kalki Nethaji <kalki@skaas.guru>
2022-09-12 20:33:29 +0530

    Generated Profile Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProfileComponent,
     HeaderComponent,
     FooterComponent
   ],
