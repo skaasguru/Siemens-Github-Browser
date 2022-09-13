a669ed0555f6c7f7e0c9354ab3416a3576ceff20
Kalki Nethaji <kalki@skaas.guru>
2022-09-14 00:16:59 +0530

    Add keyword to query param by injecting Route

--- src/app/profile-list/profile-list.component.ts
+++ src/app/profile-list/profile-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { CommonService } from '../common.service';
 import { GithubApiService } from '../github-api.service';
@@ -15,25 +16,44 @@ export class ProfileListComponent implements OnInit, OnDestroy {
 
   searchKeywordEmitterSubscription!: Subscription;
 
-  constructor(private api: GithubApiService, private commonService: CommonService) {}
+  constructor(
+    private api: GithubApiService,
+    private commonService: CommonService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
-    this.api.listUsers()
-      .then(profiles => this.profiles = profiles)
-      .catch(console.error);
+    const queryParams = this.activatedRoute.snapshot.queryParams;
+    this.getUsers(queryParams['keyword'] || '');
 
     this.searchKeywordEmitterSubscription = this.commonService.searchKeywordEmitter.subscribe((keyword: string) => {
-      let usersPromise: Promise<Profile[]>;
-      if (keyword) {
-        usersPromise = this.api.searchUsers(keyword);
-      } else {
-        usersPromise = this.api.listUsers();
-      }
-
-      usersPromise
-        .then(profiles => this.profiles = profiles)
-        .catch(console.error);
+      this.updateQueryParam({keyword})
+      this.getUsers(keyword);
     });
+  }
+
+  getUsers(keyword: string): void {
+    let usersPromise: Promise<Profile[]>;
+    if (keyword) {
+      usersPromise = this.api.searchUsers(keyword);
+    } else {
+      usersPromise = this.api.listUsers();
+    }
+
+    usersPromise
+      .then(profiles => this.profiles = profiles)
+      .catch(console.error);
+  }
+
+  updateQueryParam(queryParams: Record<string, string>) {
+    this.router.navigate([],
+      {
+        relativeTo: this.activatedRoute,
+        queryParams: queryParams,
+        queryParamsHandling: 'merge', // Remove to replace all query params by provided
+      }
+    );
   }
 
   ngOnDestroy(): void {
