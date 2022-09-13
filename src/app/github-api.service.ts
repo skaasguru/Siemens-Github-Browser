f9e32ab8cff3b59492a89abeaad58fb382362c8c
Kalki Nethaji <kalki@skaas.guru>
2022-09-13 23:33:44 +0530

    Added caching in Service

--- src/app/github-api.service.ts
+++ src/app/github-api.service.ts
@@ -6,15 +6,24 @@ import { Profile } from './types';
 })
 export class GithubApiService {
 
+  searchUsersCache: Record<string, Profile[]> = {};
+  listUsersCache: Profile[] = [];
+
   async listUsers(): Promise<Profile[]> {
+    if (this.listUsersCache.length > 0) return this.listUsersCache;
+
     const response = await fetch('https://api.github.com/users')
     const data: Profile[] = await response.json();
+    this.listUsersCache = data;
     return data;
   }
 
   async searchUsers(keyword: string): Promise<Profile[]> {
+    if (keyword in this.searchUsersCache) return this.searchUsersCache[keyword];
+
     const response = await fetch(`https://api.github.com/search/users?q=${keyword}`)
     const data: {items: Profile[]} = await response.json();
+    this.searchUsersCache[keyword] = data.items
     return data.items;
   }
 }
